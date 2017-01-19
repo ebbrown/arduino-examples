636bd66858d5a93e5867ace9e18a47c06966e162
ebbrown <ebbslo@aol.com>
2017-01-18 21:01:23 -0800

    Move Print Stuff into Separate Function

--- color_mixing_lamp/color_mixing_lamp/color_mixing_lamp.ino
+++ color_mixing_lamp/color_mixing_lamp/color_mixing_lamp.ino
@@ -1,3 +1,5 @@
+void printColorValues(String, int, int, int);
+
 const int GREEN_LED_PIN = 9, RED_LED_PIN = 10, BLUE_LED_PIN = 11;
 const int RED_SENSOR_PIN = A0, GREEN_SENSOR_PIN = A1, BLUE_SENSOR_PIN = A2;
 
@@ -18,26 +20,27 @@ void loop() {
   greenSensorValue = analogRead(GREEN_SENSOR_PIN);
   delay(5);
   blueSensorValue = analogRead(BLUE_SENSOR_PIN);
-  
-  Serial.print("Raw Sensor Values \t Red: ");
-  Serial.print(redSensorValue);
-  Serial.print("\t Green: ");
-  Serial.print(greenSensorValue);
-  Serial.print("\t Blue: ");
-  Serial.println(blueSensorValue);
+
+  printColorValues("Raw", redSensorValue, greenSensorValue, blueSensorValue);
   
   redValue = redSensorValue/4;
   greenValue = greenSensorValue/4;
   blueValue = blueSensorValue/4;
-  
-  Serial.print("Mapped Sensor Value \t Red: ");
-  Serial.print(redValue);
-  Serial.print("\t Green: ");
-  Serial.print(greenValue);
-  Serial.print("\t Blue: ");
-  Serial.println(blueValue);
+
+  printColorValues("Mapped", redValue, greenValue, blueValue);
 
   analogWrite(RED_LED_PIN, redValue);
   analogWrite(GREEN_LED_PIN, greenValue);
   analogWrite(BLUE_LED_PIN, blueValue);
 }
+
+void printColorValues(String printType, int redVal, int greenVal, int blueVal) {
+  Serial.print(printType);
+  Serial.print(" Sensor Value \t Red: ");
+  Serial.print(redVal);
+  Serial.print("\t Green: ");
+  Serial.print(greenVal);
+  Serial.print("\t Blue: ");
+  Serial.println(blueVal);
+}
+
